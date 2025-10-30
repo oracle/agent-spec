cdbb104fbd8372ad3bc727905738313b38d4dd8b
Huyen Le <huyen.l.le@oracle.com>
2025-10-30 09:08:11 +0000

    Fix docs issues

--- docs/pyagentspec/source/api/agent_specialization.rst
+++ docs/pyagentspec/source/api/agent_specialization.rst
@@ -1,19 +1,15 @@
-AgentSpecialization
-===================
+Agent Specialization
+====================
 
-This page presents all APIs and classes related to AgentSpecialization in PyAgentSpec
+This page presents all APIs and classes related to Agent Specialization in PyAgentSpec
 
 Specialized Agent
 -----------------
 
 .. _specialized_agent:
-.. autoclass:: pyagentspec.agent_specialization.SpecializedAgent
+.. autoclass:: pyagentspec.specialized_agent.SpecializedAgent
     :exclude-members: model_post_init, model_config
 
-
-Agent Specialization Parameters
--------------------------------
-
 .. _agent_specialization_parameters:
-.. autoclass:: pyagentspec.agent_specialization.AgentSpecializationParameters
+.. autoclass:: pyagentspec.specialized_agent.AgentSpecializationParameters
     :exclude-members: model_post_init, model_config
--- pyagentspec/tests/validation/test_agentic_patterns_validation.py
+++ pyagentspec/tests/validation/test_agentic_patterns_validation.py
@@ -7,6 +7,11 @@
 import pytest
 
 from pyagentspec.agent import Agent
+from pyagentspec.flows.edges.controlflowedge import ControlFlowEdge
+from pyagentspec.flows.edges.dataflowedge import DataFlowEdge
+from pyagentspec.flows.flow import Flow
+from pyagentspec.flows.nodes.endnode import EndNode
+from pyagentspec.flows.nodes.startnode import StartNode
 from pyagentspec.llms import OpenAiConfig
 from pyagentspec.managerworkers import ManagerWorkers
 from pyagentspec.swarm import Swarm
@@ -39,6 +44,38 @@ def test_managerworkers_with_manager_as_a_worker_raises_errors() -> None:
             name="managerworkers",
             group_manager=manager_agent,
             workers=[manager_agent],
+        )
+
+
+def test_managerworkers_with_different_agentic_components_can_be_validated() -> None:
+    agent = Agent(
+        name="manager_agent",
+        system_prompt="You are a group manager.",
+        llm_config=OpenAiConfig(name="default", model_id="test_model"),
+    )
+
+    start_node = StartNode(name="start_node")
+    end_node = EndNode(name="end_node")
+    flow = Flow(
+        name="flow",
+        start_node=start_node,
+        nodes=[start_node, end_node],
+        control_flow_connections=[
+            ControlFlowEdge(name="edge", from_node=start_node, to_node=end_node)
+        ],
+    )
+
+    _ = ManagerWorkers(
+        name="managerworkers",
+        group_manager=agent,
+        workers=[flow],
+    )
+
+    with pytest.raises(ValueError, match=("Group manager cannot be a worker")):
+        ManagerWorkers(
+            name="managerworkers",
+            group_manager=agent,
+            workers=[flow, agent],
         )
 
 
