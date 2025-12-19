f992576c21e2581be8937e88ac73e62d99949dbe
Youssef Boughizane <youssef.boughizane@oracle.com>
2025-12-19 13:52:09 +0100

    doc: add header comments

--- pyagentspec/src/pyagentspec/transforms/transforms.py
+++ pyagentspec/src/pyagentspec/transforms/transforms.py
@@ -3,7 +3,7 @@
 # This software is under the Apache License 2.0
 # (LICENSE-APACHE or http://www.apache.org/licenses/LICENSE-2.0) or Universal Permissive License
 # (UPL) 1.0 (LICENSE-UPL or https://oss.oracle.com/licenses/upl), at your option.
-
+"""This file defines the base MessageTransform"""
 from pyagentspec.component import Component
 
 
