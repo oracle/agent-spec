f893da370d35340912efa13e1f8d3bf4cd6822e5
Louis Faucon <louis.faucon@oracle.com>
2026-02-18 14:51:19 +0100

    fix: remove unused import to typing.Optional

--- pyagentspec/src/pyagentspec/tools/toolbox.py
+++ pyagentspec/src/pyagentspec/tools/toolbox.py
@@ -7,8 +7,6 @@
 """This module defines the base class for toolboxes."""
 
 
-from typing import Optional
-
 from pyagentspec.component import Component
 from pyagentspec.versioning import AgentSpecVersionEnum
 
