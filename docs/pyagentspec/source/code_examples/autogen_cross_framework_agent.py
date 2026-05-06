304e1e034f60ee1491387a33a796770efd7a86c1
Cesare Bernardis <cesare.bernardis@oracle.com>
2026-05-06 10:56:00 +0200

    Fix docs example

--- docs/pyagentspec/source/code_examples/autogen_cross_framework_agent.py
+++ docs/pyagentspec/source/code_examples/autogen_cross_framework_agent.py
@@ -52,7 +52,7 @@ def convert_agentspec_llm_to_autogen(agentspec_llm: LlmConfig):
         return OpenAIChatCompletionClient(
             model=agentspec_llm.model_id,
             base_url=f"http://{agentspec_llm.url}/v1",
-            api_key="",
+            api_key=agentspec_llm.api_key,
             model_info={
                 "vision": False,
                 "function_calling": True,
